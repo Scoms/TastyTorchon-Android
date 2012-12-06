645180145ea9e457668fdb0d78dd649c849aa445
MrMeme <noeldeutschmann@gmail.com>
2012-12-06 23:32:53 +0100

    Modif des classes

--- MainActivity/gen/com/example/tastydroid/R.java
+++ MainActivity/gen/com/example/tastydroid/R.java
@@ -18,8 +18,13 @@ public final class R {
         public static final int btn_geo=0x7f070004;
         public static final int btn_scan=0x7f070003;
         public static final int btn_search=0x7f070002;
-        public static final int btn_valid_circuit=0x7f070005;
-        public static final int menu_settings=0x7f070006;
+        public static final int btn_valid_circuit=0x7f07000a;
+        public static final int menu_settings=0x7f07000b;
+        public static final int tableRow1=0x7f070005;
+        public static final int tableRow2=0x7f070007;
+        public static final int tableRow3=0x7f070008;
+        public static final int tableRow4=0x7f070009;
+        public static final int textView1=0x7f070006;
         public static final int tv_search=0x7f070001;
     }
     public static final class layout {
