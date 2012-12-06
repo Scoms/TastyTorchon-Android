d98b67b3b42cf9f8d007214282a830ae213cc59c
Steve <steve@chtive-Pc>
2012-12-07 00:23:59 +0100

    add Scan

--- MainActivity/src/com/example/tastydroid/Scan.java
+++ MainActivity/src/com/example/tastydroid/Scan.java
@@ -2,6 +2,9 @@ package com.example.tastydroid;
 
 
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,16 +15,16 @@ public class Scan extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.circuit_current);
-//		IntentIntegrator integrator = new IntentIntegrator(Scan.this);  
-//        integrator.initiateScan(); 
+		IntentIntegrator integrator = new IntentIntegrator(Scan.this);  
+        integrator.initiateScan(); 
 	}
 	
-//	public void onActivityResult(int requestCode, int resultCode, Intent intent) {  
-//        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);  
-//        if (scanResult != null) {  
-//            Toast.makeText(getApplicationContext(), "resultCode: " + resultCode + " -> " + scanResult.getContents() + " " + scanResult.getFormatName(), Toast.LENGTH_LONG).show();  
-//        }  
-//    }  
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {  
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);  
+        if (scanResult != null) {  
+            Toast.makeText(getApplicationContext(), "resultCode: " + resultCode + " -> " + scanResult.getContents() + " " + scanResult.getFormatName(), Toast.LENGTH_LONG).show();  
+        }  
+    }  
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
