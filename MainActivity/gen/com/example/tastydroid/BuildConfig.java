11c107a514c90fac91479a0ac8c74523c9be43d0
Pigloo <ludo.bailly@gmail.com>
2012-12-06 21:10:11 +0100

    Add Hello World TastyDroid

---
+++ MainActivity/gen/com/example/tastydroid/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.tastydroid;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ MainActivity/src/com/example/tastydroid/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.tastydroid;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.DialogInterface.OnClickListener;
+import android.content.SharedPreferences.Editor;
+import android.view.Menu;
+import android.widget.RadioButton;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		//Load the SharedPreferences
+				SharedPreferences prefs = getSharedPreferences("TastyDroidPref",
+						MODE_PRIVATE);				
+				if (prefs.getInt("firstOpen", 0) == 0) {
+					Builder alertbox = new Builder(this);
+					alertbox.setMessage("Voulez vous voir le didacticiel ?");
+					alertbox.setPositiveButton("Yes", new OnClickListener() {
+						public void onClick(DialogInterface arg0, int arg1) {
+							Intent launchTutorial;							
+							launchTutorial = new Intent(getBaseContext(), Tutorial.class);
+							startActivity(launchTutorial);
+						}
+					});
+					alertbox.setNegativeButton("No",
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface arg0, int arg1) {
+									
+								}
+							});
+					alertbox.show();
+				}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
