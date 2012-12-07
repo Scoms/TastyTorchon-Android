78c5c9674cf4bba9728f7b4c46bb6530acb2120b
Pigloo <ludo.bailly@gmail.com>
2012-12-07 01:21:30 +0100

    Unsafe commit : set map interface / Need test on real phone Added maps lib

--- MainActivity/src/com/example/tastydroid/Geoloc.java
+++ MainActivity/src/com/example/tastydroid/Geoloc.java
@@ -1,13 +1,19 @@
 package com.example.tastydroid;
 
+import com.google.android.maps.MapActivity;
+
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Geoloc extends Activity {
+public class Geoloc extends MapActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.geoloc);
 		
 	}
 
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
 }
